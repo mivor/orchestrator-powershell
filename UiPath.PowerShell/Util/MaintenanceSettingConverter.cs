ba302138499dba83f2ded2ded2565a9f775b0d02
Radu Serbanescu <radu.serbanescu@uipath.com>
2019-08-20 15:03:25 -0700

    Maintenance Mode: PowerShell support
    [OR-8287]
    [OR-12938]
    [OR-14794]

---
+++ UiPath.PowerShell/Util/MaintenanceSettingConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UiPath.Web.Client201910.Models;
+
+namespace UiPath.PowerShell.Util
+{
+    /// <summary>
+    /// Converts maintenance logs into a dynamic objects
+    /// </summary>
+    public static class MaintenanceSettingConverter
+    {
+        private static dynamic resultTyped = new
+        {
+            State = "",
+            MaintenanceLogs = new[] { new { State = "", TimeStamp = new Nullable<DateTime>(DateTime.Now) } },
+            JobsStopped = 0,
+            JobsKilled = 0,
+            SchedulesFired = 0,
+            SystemSchedulesFired = 0
+        };
+
+        public static dynamic ToDynamic(this MaintenanceSetting input)
+        {
+            var inputJson = Newtonsoft.Json.JsonConvert.SerializeObject(input);
+            return Newtonsoft.Json.JsonConvert.DeserializeAnonymousType(inputJson, resultTyped);
+        }
+    }
+}
